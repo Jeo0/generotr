8f8ea52c9b13a103f2f3f71c25d9368d0c32cd75
Jeo0 <jjrelb@gmail.com>
2023-10-15 22:14:19 +0800

    working servo lib - just go broombroom and c the broombroom

--- Generator/scripts/potentiometer/potentiometer.ino
+++ Generator/scripts/potentiometer/potentiometer.ino
@@ -17,6 +17,7 @@
 // https://www.electronicshub.org/esp32-servo-control/
 // https://microcontrollerslab.com/esp32-pwm-arduino-ide-led-fading-example/
 // https://esp32.com/viewtopic.php?t=20450
+// https://desktopmake.hateblo.jp/entry/2022/11/13/152713
 // pin 25
 
 /* lcd: https://www.circuitschools.com/interfacing-16x2-lcd-module-with-esp32-with-and-without-i2c/
@@ -41,15 +42,27 @@
 #define Poten_pin 36
 #define ESC_pin 25
 #define Hall_pin 26
+#define MIN_SPEED 0 // speed just slow enough to turn motor off
+#define MAX_SPEED 100 // speed where my motor drew 3.6 amps at 12v.
+
 
 #include <LiquidCrystal.h>
+#include <ESP32Servo.h>     // ESP32Servo library installed by Library Manager
+//#include "ESC.h"            // RC_ESP library installed by Library Manager
+//#include <Servo.h>
  
 // initialize the library with the numbers of the interface pins
+// lcd
 LiquidCrystal lcd(19, 23, 18, 17, 16, 15);
-
 float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+
+
+
+
+Servo ESC;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -61,6 +74,17 @@ void setup() {
   lcd.begin(16, 2);
   // Print a message to the LCD.
   //lcd.print("circuitschools.");
+
+  pinMode(Poten_pin, INPUT);
+
+    //Brushlessmotor
+  pinMode(ESC_pin, OUTPUT);//ESC_pin as OUTPUT
+  //ledcSetup(0, 1000, 8);//PWM channel is 0, Frequency 1000Hz, 8 bit(Duty with 256 levels of resolution, the value is 0-255)
+  //ledcAttachPin(ESC_pin,0);//Assign channel 0 to ESC_pin
+
+  ESC.attach(ESC_pin, 1000, 2000);
+
+
 }
 
 
@@ -74,10 +98,10 @@ void loop() {
   int analogValue = analogRead(Poten_pin);
   // Rescale to potentiometer's voltage (from 0V to 3.3V):
   //float voltage = floatMap(analogValue, 0, 4095, 0, 3.3);
-  float voltage = floatMap(analogValue, 0, 4095, 0, 5);
-  float percent = floatMap(analogValue, 0, 4095, 0, 100);
-  float v5 = floatMap(voltage, 0, 3.3, 0, 5);
-  float p5 = floatMap(percent, 0, 100, 0, 100);
+  float voltage = floatMap(analogValue, 0, 4095, 0, 3.3);
+  float percent = floatMap(analogValue, 0, 4095, MIN_SPEED, MAX_SPEED);
+  // float v5 = floatMap(voltage, 0, 3.3, 0, 5);
+  // float p5 = floatMap(percent, 0, 100, 0, 100);
 
   // print out the value you read:
   Serial.print("Analog: ");
@@ -88,7 +112,7 @@ void loop() {
   Serial.print(": ");
   Serial.println(percent);
   Serial.println("%");
-  delay(250);
+  delay(1);
   
 
   ///////////////////////////////////////////////
@@ -102,6 +126,16 @@ void loop() {
   lcd.print(voltage);
   lcd.setCursor(0, 0);
   lcd.print(percent);
+
+
+  // for esc
+  //uint8_t recv_data[6];
+  //myESC.speed(val); // sets the ESC speed
+  //delay(10); // Wait for a while
+
+  float esc_out = map(percent, MIN_SPEED, MAX_SPEED, 0, 180);
+  ESC.write(esc_out);
+
 
 
 }
